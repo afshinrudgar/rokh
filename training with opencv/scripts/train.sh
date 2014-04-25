7a2d9d82ea6d701c59b3cfcbad79b6839c4b2eae
Afshin Rodgar <afshin.chichi@gmail.com>
2014-04-26 00:40:51 +0430

    just a little more change

--- training with opencv/scripts/train.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-opencv_traincascade -data classifier -vec samples.vec -bg negatives.txt -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 100 -numNeg 54 -w 40 -h 40 -mode ALL -precalcValBufSize 512 -precalcIdxBufSize 512
